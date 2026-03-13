2a46a64a798f5ac6340f375e40ba8fd4a555feff
wiroger9595@gmail.com <wiroger9595@gmail.com>
2026-03-13 18:15:24 +0800

    fix google auth

--- server/app/main.py
+++ server/app/main.py
@@ -15,6 +15,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:[0-9]+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
